23b48fcde28ff2116418bba4a975a29eec04adf5
mahdizaabi <mahdi.zaabi@gmail.com>
2020-09-27 21:32:16 +0000

    add

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,13 @@
+const listAmen = [];
 $(document).ready(function () {
   $('input[type="checkbox"]').click(function () {
-    const inputCheck = $('input[type="checkbox"]');
-    const listAmen = [];
-    if (inputCheck.length > 0) {
-      listAmen.push('{{ amenity.id }}');
+    if (this.checked) {
+      console.log($(this).attr('data-id'));
+      listAmen.push($(this).attr('data-id'));
     } else {
-      listAmen.splice(listAmen.indexOf('{{ amenity.id }}'), 1);
+      listAmen.splice(listAmen.indexOf($(this).attr('data-id'), 1));
     }
+    $('.amenities h4').css('display', 'inline-block');
+    $('.amenities h4').append($(this).attr('data-name') + ' ');
   });
 });
