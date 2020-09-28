83430b4033f13f095e38ed74f7a9dc934d15ac49
crxs10 <59511107+mahdizaabi@users.noreply.github.com>
2020-09-28 04:49:28 +0200

    Update 2-hbnb.js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -13,12 +13,10 @@ $(document).ready(function () {
 });
 
 $.get('http://55.55.55.5:5001/api/v1/status/', function (data, response) {
-    if (response === 'success') {      
-        console.log('succes');
+    if (response === 'success') {
         $('DIV#api_status').css("background-color", "#ff545f");
         //$('#api_status').addClass('available');
       } else {
-        console.log('fail');
         $('DIV#api_status').removeClass('available');
       }
     
